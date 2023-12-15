fe03c274068d49a6b5668cc9049fa2b293659a54
Michael Zalla <michael@zalla.io>
2023-12-15 14:14:48 -0800

    Annotates OBJ line element types;
    Prints unrecognized types when encountered;

--- src/mesh/obj/mod.rs
+++ src/mesh/obj/mod.rs
@@ -30,8 +30,12 @@ pub fn get_mesh_from_obj(filepath: String) -> Mesh {
                     None => (),
                     Some(first) => {
                         match first {
+                            // Comment
+                            "#" => (),
+                            // Geometric vertex, with (x, y, z, [w]) coordinates, w is optional and defaults to 1.0.
                             "v" => {
-                                // `v  -0.512365 -40.559704 21.367237`
+                                // `v  -0.512365 -40.559704 21.367237` (x y z)
+                                // `v  -0.512365 -40.559704 21.367237 50 255 0` (x y z r g b)
 
                                 let (x, y, z) = (
                                     line_components.next().unwrap().parse::<f32>().unwrap(),
@@ -41,8 +45,36 @@ pub fn get_mesh_from_obj(filepath: String) -> Mesh {
 
                                 vertices.push(Vec3 { x, y, z });
                             }
+                            // Texture (UV) coordinate, as (u, [v, w]), between 0 and 1. v, w are optional and default to 0.
+                            "vt" => (),
+                            // Vertex normal in (x,y,z) form; normal might not be a unit vector.
+                            "vn" => {
+                                // `vn  0.000005 -34.698460 -17.753405` (x y z)
+
+                                let vertex_normal = Vec3 {
+                                    x: line_components.next().unwrap().parse::<f32>().unwrap(),
+                                    y: line_components.next().unwrap().parse::<f32>().unwrap(),
+                                    z: line_components.next().unwrap().parse::<f32>().unwrap(),
+                                };
+
+                                vertex_normals.push(vertex_normal);
+                            }
+                            // Parameter space vertex
+                            "vp" => (),
+                            // Polygonal face
                             "f" => {
-                                // `f 1004//1004 1003//1003 1002//1002`
+                                // Vertex indices only:             f v1 v2 v3 ....
+                                // Vertex and UV indices:           f v1/vt1 v2/vt2 v3/vt3 ...
+                                // Vertex, UV, and normal indices:  f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3 ...
+                                // Vertex and normal indices only:  f v1//vn1 v2//vn2 v3//vn3 ...
+
+                                // f 1 2 3
+                                // f 3/1 4/2 5/3
+                                // f 6/4/1 3/5/3 7/6/5
+                                // f 7//1 8//2 9//3
+
+                                // `f 1004//1004 1003//1003 1002//1002` ({x,y,z}{vert_index, texture_index, vert_normal_index})
+                                // `f 1004//1004 1003//1003 1002//1002` ({x,y,z}{vert_index, texture_index, vert_normal_index})
 
                                 let mut x = line_components.next().unwrap().split("/");
                                 let mut y = line_components.next().unwrap().split("/");
@@ -70,25 +102,21 @@ pub fn get_mesh_from_obj(filepath: String) -> Mesh {
                                     None => (),
                                 }
                             }
-                            "vn" => {
-                                // `vn  0.000005 -34.698460 -17.753405`
-
-                                let vertex_normal = Vec3 {
-                                    x: line_components.next().unwrap().parse::<f32>().unwrap(),
-                                    y: line_components.next().unwrap().parse::<f32>().unwrap(),
-                                    z: line_components.next().unwrap().parse::<f32>().unwrap(),
-                                };
-
-                                vertex_normals.push(vertex_normal);
-                            }
-                            "#" => (),
-                            // "mtllib" => println!("mtllib"),
-                            // "ysentk" => println!("ysentk"),
-                            // "o" => println!("o"),
-                            // "g" => println!("g"),
-                            // "s" => println!("s"),
-                            _ => {
-                                // println!("{}", other)
+                            // Line element
+                            "l" => (),
+                            // External material reference
+                            "mtllib" => (),
+                            // Material group
+                            "usemtl" => (),
+                            // Named object
+                            "o" => (),
+                            // Named object polygon group
+                            "g" => (),
+                            // Polygon smoothing group
+                            "s" => (),
+                            // Unrecognized prefix
+                            other => {
+                                println!("{}", other)
                             }
                         }
                     }
