26d7a839bdaa2d54618659f9e9bada6381f0ad66
Michael Zalla <michael@zalla.io>
2023-12-19 18:00:11 -0800

    Renames field;

--- src/material/mod.rs
+++ src/material/mod.rs
@@ -2,7 +2,7 @@ use crate::{image::TextureMap, vec::vec3::Vec3};
 
 #[derive(Debug, Clone, Default)]
 pub struct Material {
-    pub diffuse: Vec3,
+    pub diffuse_color: Vec3,
     pub diffuse_map: Option<TextureMap>,
     pub normal_map: Option<TextureMap>,
 }
