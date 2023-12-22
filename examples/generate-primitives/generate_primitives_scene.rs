fe828124660c173a12c5d468f33e50fe4ab2de58
Michael Zalla <michael@zalla.io>
2023-12-22 10:04:22 -0800

    Renames fields;

--- examples/generate-primitives/generate_primitives_scene.rs
+++ examples/generate-primitives/generate_primitives_scene.rs
@@ -29,8 +29,8 @@ static PROJECTION_Z_FAR: f32 = 100.0;
 pub struct GeneratePrimitivesScene<'a> {
     pipeline: Pipeline<DefaultEffect>,
     pipeline_options: PipelineOptions,
-    viewport_width: u32,
-    viewport_height: u32,
+    canvas_width: u32,
+    canvas_height: u32,
     cameras: Vec<Camera>,
     active_camera_index: usize,
     // ambient_light: AmbientLight,
@@ -53,12 +53,12 @@ impl<'a> GeneratePrimitivesScene<'a> {
             .output_size()
             .unwrap();
 
-        let viewport_width = canvas_output_size.0;
-        let viewport_height = canvas_output_size.1;
-        let aspect_ratio = viewport_width as f32 / viewport_height as f32;
+        let canvas_width = canvas_output_size.0;
+        let canvas_height = canvas_output_size.1;
+        let aspect_ratio = canvas_width as f32 / canvas_height as f32;
 
         let graphics = Graphics {
-            buffer: PixelBuffer::new(viewport_width, viewport_height),
+            buffer: PixelBuffer::new(canvas_width, canvas_height),
         };
 
         // Set up a camera for rendering our cube scene
@@ -161,8 +161,8 @@ impl<'a> GeneratePrimitivesScene<'a> {
             // ambient_light,
             directional_light,
             point_light,
-            viewport_width,
-            viewport_height,
+            canvas_width,
+            canvas_height,
             prev_mouse_state: MouseState::new(),
         };
     }
@@ -180,12 +180,11 @@ impl<'a> Scene for GeneratePrimitivesScene<'a> {
 
         let mouse_position = mouse_state.position;
 
-        let ndc_mouse_x = mouse_position.0 as f32 / self.viewport_width as f32;
-        let ndc_mouse_y = mouse_position.1 as f32 / self.viewport_height as f32;
+        let ndc_mouse_x = mouse_position.0 as f32 / self.canvas_width as f32;
+        let ndc_mouse_y = mouse_position.1 as f32 / self.canvas_height as f32;
 
-        let prev_ndc_mouse_x = self.prev_mouse_state.position.0 as f32 / self.viewport_width as f32;
-        let prev_ndc_mouse_y =
-            self.prev_mouse_state.position.1 as f32 / self.viewport_height as f32;
+        let prev_ndc_mouse_x = self.prev_mouse_state.position.0 as f32 / self.canvas_width as f32;
+        let prev_ndc_mouse_y = self.prev_mouse_state.position.1 as f32 / self.canvas_height as f32;
 
         let mouse_x_delta = ndc_mouse_x - prev_ndc_mouse_x;
         let mouse_y_delta = ndc_mouse_y - prev_ndc_mouse_y;
