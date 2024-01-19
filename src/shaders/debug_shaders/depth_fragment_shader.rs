748f8c8f578bc361cbeae567c822f2d1641bd6c3
Michael Zalla <michael@zalla.io>
2024-01-18 22:34:08 -0800

    Maps foreground depth values into a better visual range;

--- src/shaders/debug_shaders/depth_fragment_shader.rs
+++ src/shaders/debug_shaders/depth_fragment_shader.rs
@@ -45,13 +45,21 @@ impl<'a> FragmentShader<'a> for DepthFragmentShader<'a> {
         //               1/z = nlz * (1/f - 1/n) + 1/n
         //                 z = 1 / (nlz * (1/f - 1/n) + 1/n)
 
-        let linear_depth = 1.0 / (non_linear_depth * (1.0 / 10.0 - 1.0 / 0.3) + 1.0 / 0.3);
+        static NEAR: f32 = 0.3;
+        static FAR: f32 = 1000.0;
+
+        let ndc = non_linear_depth * 2.0 - 1.0;
+
+        let linear_depth = (2.0 * NEAR * FAR) / (FAR + NEAR - ndc * (FAR - NEAR));
+
+        // [0, 1] -> [10, 0]
+        let adjusted_linear_depth = 10.0 - (linear_depth * 10.0);
 
         // @NOTE May need to account for diplacement map in the future.
         return Color {
-            r: ((1.0 - linear_depth) * 255.0) as u8,
-            g: ((1.0 - linear_depth) * 255.0) as u8,
-            b: ((1.0 - linear_depth) * 255.0) as u8,
+            r: (adjusted_linear_depth * 255.0) as u8,
+            g: (adjusted_linear_depth * 255.0) as u8,
+            b: (adjusted_linear_depth * 255.0) as u8,
             a: 255 as u8,
         };
     }
