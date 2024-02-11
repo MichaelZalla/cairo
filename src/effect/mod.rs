658b0cdff0fef7fa49ccdcb60229bf4f35547fb8
Michael Zalla <michael@zalla.io>
2024-02-11 12:25:41 -0800

    Adds type parameter to `Effect` trait;

--- src/effect/mod.rs
+++ src/effect/mod.rs
@@ -1,7 +1,9 @@
+use std::{fmt::Debug, ops::Add};
+
 use crate::buffer::Buffer2D;
 
 pub mod kernel;
 
-pub trait Effect {
-    fn apply(&self, buffer: &mut Buffer2D);
+pub trait Effect<T: Default + PartialEq + Copy + Clone + Debug + Add = u32> {
+    fn apply(&self, buffer: &mut Buffer2D<T>);
 }
