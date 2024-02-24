8803c9571ba625be454b1ee1b232ed3e1b05fc7e
Michael Zalla <michael@zalla.io>
2024-02-23 19:25:21 -0800

    Moves static function out of `impl` block;

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -4,6 +4,58 @@ use super::{
     vec::vec3::Vec3,
 };
 
+fn make_bounding_box(mesh: &Mesh) -> AABB {
+    let mut x_min: f32 = f32::MAX;
+    let mut x_max: f32 = f32::MIN;
+
+    let mut y_min: f32 = f32::MAX;
+    let mut y_max: f32 = f32::MIN;
+
+    let mut z_min: f32 = f32::MAX;
+    let mut z_max: f32 = f32::MIN;
+
+    for v in mesh.vertices.as_slice() {
+        if v.x < x_min {
+            x_min = v.x;
+        } else if v.x > x_max {
+            x_max = v.x;
+        }
+
+        if v.y < y_min {
+            y_min = v.y;
+        } else if v.y > y_max {
+            y_max = v.y;
+        }
+
+        if v.z < z_min {
+            z_min = v.z;
+        } else if v.z > z_max {
+            z_max = v.z;
+        }
+    }
+
+    let width = x_max - x_min;
+    let height = y_max - y_min;
+    let depth = z_max - z_min;
+
+    let result = AABB {
+        center: Vec3 {
+            x: x_min + width / 2.0,
+            y: y_min + height / 2.0,
+            z: z_min + depth / 2.0,
+        },
+        half_dimension: (x_max - x_min) / 2.0,
+        left: x_min,
+        right: x_max,
+        top: y_max,
+        bottom: y_min,
+        near: z_max,
+        far: z_min,
+    };
+
+    result
+}
+
 #[derive(Clone)]
 pub struct Entity<'a> {
     pub position: Vec3,
@@ -15,7 +67,7 @@ pub struct Entity<'a> {
 
 impl<'a> Entity<'a> {
     pub fn new(mesh: &'a Mesh) -> Self {
-        let bounds = Entity::make_bounding_box(&mesh);
+        let bounds = make_bounding_box(&mesh);
         let bounds_mesh = Entity::make_bounding_box_mesh(&bounds);
 
         Entity {
@@ -25,58 +77,6 @@ impl<'a> Entity<'a> {
             bounds,
             bounds_mesh,
         }
-    }
-
-    fn make_bounding_box(mesh: &Mesh) -> AABB {
-        let mut x_min: f32 = f32::MAX;
-        let mut x_max: f32 = f32::MIN;
-
-        let mut y_min: f32 = f32::MAX;
-        let mut y_max: f32 = f32::MIN;
-
-        let mut z_min: f32 = f32::MAX;
-        let mut z_max: f32 = f32::MIN;
-
-        for v in mesh.vertices.as_slice() {
-            if v.x < x_min {
-                x_min = v.x;
-            } else if v.x > x_max {
-                x_max = v.x;
-            }
-
-            if v.y < y_min {
-                y_min = v.y;
-            } else if v.y > y_max {
-                y_max = v.y;
-            }
-
-            if v.z < z_min {
-                z_min = v.z;
-            } else if v.z > z_max {
-                z_max = v.z;
-            }
-        }
-
-        let width = x_max - x_min;
-        let height = y_max - y_min;
-        let depth = z_max - z_min;
-
-        let result = AABB {
-            center: Vec3 {
-                x: x_min + width / 2.0,
-                y: y_min + height / 2.0,
-                z: z_min + depth / 2.0,
-            },
-            half_dimension: (x_max - x_min) / 2.0,
-            left: x_min,
-            right: x_max,
-            top: y_max,
-            bottom: y_min,
-            near: z_max,
-            far: z_min,
-        };
-
-        return result;
     }
 
     pub fn make_bounding_box_mesh(bounds: &AABB) -> Mesh {
