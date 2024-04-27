fe2b2611ed13c825fa01c51bb1ce3dd7fd3c10e4
Michael Zalla <michael@zalla.io>
2024-04-26 19:13:52 -0700

    Adds `Hdr::to_vec3()` method;

--- examples/ibl/main.rs
+++ examples/ibl/main.rs
@@ -1,10 +1,6 @@
 use std::path::Path;
 
-use cairo::{
-    buffer::Buffer2D,
-    hdr::{load::load_hdr, rgbe::Rgbe, HDR_CHANNELS_PER_SAMPLE},
-    vec::vec3::Vec3,
-};
+use cairo::{buffer::Buffer2D, hdr::load::load_hdr, vec::vec3::Vec3};
 
 fn main() -> Result<(), String> {
     let filepath = Path::new("./examples/ibl/assets/rural_asphalt_road_4k.hdr");
@@ -15,26 +11,7 @@ fn main() -> Result<(), String> {
             println!("{:?}", hdr.headers);
             println!("Decoded {} bytes from file.", hdr.bytes.len());
 
-            let chunks = hdr.bytes.chunks(4);
-
-            let vecs = {
-                let mut vecs =
-                    Vec::<Vec3>::with_capacity(hdr.bytes.len() / HDR_CHANNELS_PER_SAMPLE);
-
-                let _ = chunks.map(|bytes| {
-                    vecs.push(
-                        Rgbe {
-                            r: bytes[0],
-                            g: bytes[1],
-                            b: bytes[2],
-                            e: bytes[3],
-                        }
-                        .to_vec3(),
-                    );
-                });
-
-                vecs
-            };
+            let vecs = hdr.to_vec3();
 
             let buffer = Buffer2D::<Vec3>::from_data(
                 hdr.source.width as u32,
--- src/hdr/mod.rs
+++ src/hdr/mod.rs
@@ -2,6 +2,10 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
+use crate::vec::vec3::Vec3;
+
+use self::rgbe::Rgbe;
+
 pub mod load;
 pub mod rgbe;
 
@@ -32,4 +36,24 @@ pub struct Hdr {
     pub headers: HashMap<String, String>,
     #[serde(skip)]
     pub bytes: Vec<u8>,
+}
+
+impl Hdr {
+    pub fn to_vec3(&self) -> Vec<Vec3> {
+        let mut vecs = Vec::<Vec3>::with_capacity(self.bytes.len() / HDR_CHANNELS_PER_SAMPLE);
+
+        for chunk in self.bytes.chunks(4) {
+            vecs.push(
+                Rgbe {
+                    r: chunk[0],
+                    g: chunk[1],
+                    b: chunk[2],
+                    e: chunk[3],
+                }
+                .to_vec3(),
+            );
+        }
+
+        vecs
+    }
 }
