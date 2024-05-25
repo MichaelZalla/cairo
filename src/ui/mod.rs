dc5b49a19e760c18110f1c3bb9df0e607e94f173
Michael Zalla <michael@zalla.io>
2024-05-25 14:12:14 -0700

    Semantic UI sizes (relatives to axes);

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,5 +1,30 @@
 #[derive(Default, Debug, Copy, Clone)]
-pub struct UIWidget {}
+pub enum UISize {
+    #[default]
+    Null,
+    Pixels(u32),
+    TextContent,
+    PercentOfParent(f32),
+    ChildrenSum,
+}
+
+#[derive(Default, Debug, Copy, Clone)]
+pub struct UISizeWithStrictness {
+    size: UISize,
+    strictness: f32,
+}
+
+#[derive(Default, Debug, Copy, Clone)]
+pub enum UI2DAxis {
+    #[default]
+    X,
+    Y,
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct UIWidget {
+    semantic_sizes: [UISizeWithStrictness; 2],
+}
 
 #[derive(Default, Debug, Clone)]
 pub struct UIWidgetStack {
