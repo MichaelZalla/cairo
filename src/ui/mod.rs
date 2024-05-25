77932f01e349b425c2e608f6812f08e8e7451b84
Michael Zalla <michael@zalla.io>
2024-05-25 13:59:26 -0700

    Renaming;

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,23 +1,23 @@
 #[derive(Default, Debug, Copy, Clone)]
-pub struct UILayout {}
+pub struct UIWidget {}
 
 #[derive(Default, Debug, Clone)]
-pub struct UILayoutStack {
-    stack: Vec<UILayout>,
+pub struct UIWidgetStack {
+    stack: Vec<UIWidget>,
 }
 
-impl UILayoutStack {
-    pub fn push(&mut self, layout: UILayout) {
-        self.stack.push(layout)
+impl UIWidgetStack {
+    pub fn push(&mut self, widget: UIWidget) {
+        self.stack.push(widget)
     }
 
-    pub fn pop(&mut self) -> Option<UILayout> {
+    pub fn pop(&mut self) -> Option<UIWidget> {
         self.stack.pop()
     }
 }
 
-pub struct UILayoutContext {
-    pub layouts: UILayoutStack,
+pub struct UIContext {
+    pub stack: UIWidgetStack,
 }
 
-impl UILayoutContext {}
+impl UIContext {}
