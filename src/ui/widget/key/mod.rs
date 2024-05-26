f110a20b62c27c97c34617e489ae750b85ea1795
Michael Zalla <michael@zalla.io>
2024-05-26 08:40:26 -0700

    Moves `UIKey` to module;

---
+++ src/ui/widget/key/mod.rs
@@ -0,0 +1,28 @@
+use core::fmt;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct UIKey {
+    hash: Option<String>,
+}
+
+impl fmt::Display for UIKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "UIKey(hash={})",
+            if let Some(hash) = &self.hash {
+                format!("\"{}\"", hash)
+            } else {
+                "None".to_string()
+            }
+        )
+    }
+}
+
+impl UIKey {
+    pub fn from_string(id: String) -> Self {
+        Self { hash: Some(id) }
+    }
+}
--- src/ui/widget/mod.rs
+++ src/ui/widget/mod.rs
@@ -12,32 +12,11 @@ use crate::{
     vec::vec2::Vec2,
 };
 
+use self::key::UIKey;
+
 use super::{UISizeWithStrictness, UI_2D_AXIS_COUNT};
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
-pub struct UIKey {
-    hash: Option<String>,
-}
-
-impl fmt::Display for UIKey {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "UIKey(hash={})",
-            if let Some(hash) = &self.hash {
-                format!("\"{}\"", hash)
-            } else {
-                "None".to_string()
-            }
-        )
-    }
-}
-
-impl UIKey {
-    pub fn from_string(id: String) -> Self {
-        Self { hash: Some(id) }
-    }
-}
+pub mod key;
 
 bitmask! {
     #[derive(Default, Debug, Serialize, Deserialize)]
