78d8725adca2ffc7f0171d06ee298e8447931d32
Michael Zalla <michael@zalla.io>
2024-06-04 21:06:37 -0700

    LRWETMLL: Implements `List::into_iter()`;

--- src/mem/list/mod.rs
+++ src/mem/list/mod.rs
@@ -3,6 +3,8 @@ pub struct List<T> {
     head: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
 type Link<T> = Option<Box<Node<T>>>;
 
 #[derive(Default, Debug, Clone)]
@@ -21,9 +23,21 @@ impl<T> Drop for List<T> {
     }
 }
 
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         Self { head: None }
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
     }
 
     pub fn peek(&self) -> Option<&T> {
@@ -105,5 +119,21 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
