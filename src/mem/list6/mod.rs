e32f2d3aa6df7e2d7fcd9be8453f387e549dfdf4
Michael Zalla <michael@zalla.io>
2024-06-07 14:34:05 -0700

    LRWETMLL: Implements reference-based iterators;

--- src/mem/list6/mod.rs
+++ src/mem/list6/mod.rs
@@ -5,6 +5,16 @@ pub struct List<T> {
     tail: *mut Node<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
 pub type Link<T> = *mut Node<T>;
 
 pub struct Node<T> {
@@ -18,6 +28,33 @@ impl<T> List<T> {
         Self {
             head: ptr::null_mut(),
             tail: ptr::null_mut(),
+        }
+    }
+
+    #[allow(clippy::should_implement_trait)]
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        unsafe {
+            // "For the duration of this lifetime, the memory the pointer points
+            // to must not get accessed (read or written) through any other
+            // pointer."
+            Iter {
+                next: self.head.as_ref(),
+            }
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        unsafe {
+            // "For the duration of this lifetime, the memory the pointer points
+            // to must not get accessed (read or written) through any other
+            // pointer."
+            IterMut {
+                next: self.head.as_mut(),
+            }
         }
     }
 
@@ -59,6 +96,43 @@ impl<T> List<T> {
     }
 }
 
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // self.next.take().map(|node| {
+        self.next.map(|node| {
+            unsafe {
+                self.next = node.next.as_ref();
+            }
+
+            &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            unsafe {
+                self.next = node.next.as_mut();
+            }
+
+            &mut node.elem
+        })
+    }
+}
+
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         while self.pop().is_some() {}
@@ -97,5 +171,47 @@ mod test {
         assert_eq!(list.pop(), Some(6));
         assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 1));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), None);
     }
 }
