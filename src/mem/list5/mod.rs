7068ae67f54ae7f3edbecfee4fdf68923b315d3d
Michael Zalla <michael@zalla.io>
2024-06-07 12:23:34 -0700

    LRWETMLL: Violation of stacked borrows;

---
+++ src/mem/list5/mod.rs
@@ -0,0 +1,114 @@
+use std::ptr;
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+struct List<T> {
+    head: Link<T>,
+    tail: *mut Node<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: ptr::null_mut(),
+        }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        // Push a new node to the end of our queue.
+
+        let mut new_tail = Box::new(Node { elem, next: None });
+
+        let new_tail_raw_ptr: *mut _ = &mut *new_tail;
+
+        if !self.tail.is_null() {
+            unsafe {
+                (*self.tail).next = Some(new_tail);
+            }
+        } else {
+            self.head = Some(new_tail);
+        }
+
+        self.tail = new_tail_raw_ptr;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            let head = *old_head;
+
+            self.head = head.next;
+
+            if self.head.is_none() {
+                self.tail = ptr::null_mut();
+            }
+
+            head.elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    // #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        list.push(6);
+        list.push(7);
+
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn aliasing() {
+        let mut data = 10;
+
+        let ref1 = &mut data;
+        // Now, can't mutate through `data` until we're done with `ref`.
+
+        let ref2 = &mut *ref1;
+        // Now, can't mutate through `ref1` until we're done with `ref2`.
+
+        // ERROR: cannot use `*ref1` because it was mutably borrowed use of borrowed `*ref1`
+        // *ref1 += 1;
+
+        // Last use of `ref2`.
+        *ref2 += 2;
+
+        // Last use of `ref1`.
+        *ref1 += 1;
+
+        // Okay to write to memory through `data` here.
+        data += 3;
+
+        println!("{}", data);
+    }
+}
--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -2,3 +2,4 @@ pub mod list;
 pub mod list2;
 pub mod list3;
 pub mod list4;
+pub mod list5;
