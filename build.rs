297f6f0cc0edd88bfcd2a4a2f95dc95f31081ced
Michael Zalla <michael@zalla.io>
2024-06-11 07:53:56 -0700

    Includes SDL2 DLL in linker path;

--- build.rs
+++ build.rs
@@ -9,6 +9,7 @@ fn main() {
     let short_hash = String::from_utf8(short_hash_output.stdout).unwrap();
 
     println!("cargo:rustc-env=GIT_COMMIT_SHORT_HASH={}", short_hash);
+    println!(r"cargo:rustc-link-search=C:\Users\micha\source\vclib\SDL2-2.28.5\lib\x64\");
     println!(r"cargo:rustc-link-search=C:\Users\micha\source\vclib\SDL2_ttf-2.20.2\lib\x64\");
     println!(r"cargo:rustc-link-search=C:\Users\micha\source\vclib\SDL2_image-2.8.0\lib\x64\");
 }
