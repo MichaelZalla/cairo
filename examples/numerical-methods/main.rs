fd358f8bedb4d7b50e24f1dfbab39e8c1ed6359e
Michael Zalla <michael@zalla.io>
2024-09-08 18:50:57 -0700

    Sinusoidal oscillation with Euler, RK2, and RK4;

--- examples/numerical-methods/main.rs
+++ examples/numerical-methods/main.rs
@@ -1,4 +1,4 @@
-use std::cell::RefCell;
+use std::{cell::RefCell, f32::consts::PI};
 
 use cairo::{
     app::{resolution::Resolution, App, AppWindowInfo},
@@ -49,8 +49,32 @@ fn main() -> Result<(), String> {
     let exp_decay_system_dynamics_function =
         Box::leak(Box::new(|state: State| -> StateDerivative {
             StateDerivative {
+                // v(t) = (-1 / T) * x
                 f0: (-1.0 / TIME_CONSTANT_OF_DECAY) * state.f0,
                 f1: 0.0,
+            }
+        }));
+
+    // Sinusoidal oscillation.
+
+    static TWO_PI: f32 = 2.0 * PI;
+    static MAGNITUDE: f32 = 2.0;
+    static PERIOD: f32 = 0.5 * TWO_PI;
+    static PHASE_ANGLE: f32 = 0.0;
+    static FREQUENCY: f32 = TWO_PI / PERIOD;
+
+    let oscillation: BoxedGraphingFunction =
+        Box::new(|t: f32| -> f32 { MAGNITUDE * (2.0 * PI * (t / PERIOD) - PHASE_ANGLE).cos() });
+
+    let oscillation_system_dynamics_function =
+        Box::leak(Box::new(|state: State| -> StateDerivative {
+            // E_k = (1/2) * M * V^2
+            // a(t) = -(F^2) * x - D * v
+            static DAMPENING: f32 = 0.005;
+
+            StateDerivative {
+                f0: state.f1,                                                   // Velocity
+                f1: -(FREQUENCY * FREQUENCY) * state.f0 - DAMPENING * state.f1, // Acceleration
             }
         }));
 
@@ -85,72 +109,127 @@ fn main() -> Result<(), String> {
         // graph.point(1.0, 1.0, &color::ORANGE, &mut framebuffer);
         // graph.line(0.0, 0.0, 5.0, 3.0, &color::ORANGE, &mut framebuffer);
 
-        // Graph the exact solution for exponential decay.
+        if false {
+            // Graph the exact solution for exponential decay.
 
-        graph.function(&exp_decay, &color::WHITE, &mut framebuffer);
+            graph.function(&exp_decay, &color::WHITE, &mut framebuffer);
 
-        graph.line(
-            0.0,
-            X_0,
-            TIME_CONSTANT_OF_DECAY,
-            0.0,
-            &color::DARK_GRAY,
-            &mut framebuffer,
-        );
+            graph.line(
+                0.0,
+                X_0,
+                TIME_CONSTANT_OF_DECAY,
+                0.0,
+                &color::DARK_GRAY,
+                &mut framebuffer,
+            );
 
-        // Graph approximate solutions using our different integrators.
+            // Graph approximate solutions using our different integrators.
 
-        // let step_size = 2.0 * TIME_CONSTANT_OF_DECAY;
-        let step_size = TIME_CONSTANT_OF_DECAY;
-        // let step_size = TIME_CONSTANT_OF_DECAY / 2.0;
-        // let step_size = TIME_CONSTANT_OF_DECAY / 4.0;
+            // let step_size = 2.0 * TIME_CONSTANT_OF_DECAY;
+            let step_size = TIME_CONSTANT_OF_DECAY;
+            // let step_size = TIME_CONSTANT_OF_DECAY / 2.0;
+            // let step_size = TIME_CONSTANT_OF_DECAY / 4.0;
 
-        let state_0 = State { f0: X_0, f1: 0.0 };
+            let state_0 = State { f0: X_0, f1: 0.0 };
 
-        // Approximate the solution using basic Euler (O(h^2)).
+            // Approximate the solution using basic Euler (O(h^2)).
 
-        // h <= 2*T
-        // 2*T = 5.0
+            // h <= 2*T
+            // 2*T = 5.0
 
-        integrate_euler(
-            state_0,
-            exp_decay_system_dynamics_function,
-            step_size,
-            (10.0 / step_size) as usize,
-            &graph,
-            &color::YELLOW,
-            &mut framebuffer,
-        );
+            integrate_euler(
+                state_0,
+                exp_decay_system_dynamics_function,
+                step_size,
+                (10.0 / step_size) as usize,
+                &graph,
+                &color::YELLOW,
+                &mut framebuffer,
+            );
 
-        // Approximate the solution using RK2 (O(h^3)).
+            // Approximate the solution using RK2 (O(h^3)).
 
-        // h <= 2*T
-        // 2*T = 5.0
+            // h <= 2*T
+            // 2*T = 5.0
 
-        integrate_rk2(
-            state_0,
-            exp_decay_system_dynamics_function,
-            step_size,
-            (10.0 / step_size) as usize,
-            &graph,
-            &color::GREEN,
-            &mut framebuffer,
-        );
+            integrate_rk2(
+                state_0,
+                exp_decay_system_dynamics_function,
+                step_size,
+                (10.0 / step_size) as usize,
+                &graph,
+                &color::GREEN,
+                &mut framebuffer,
+            );
 
-        // Approximate the solution using RK4 (O(h^5)).
+            // Approximate the solution using RK4 (O(h^5)).
 
-        // h <= 2.78*T
-        // 2*T = 6.95
+            // h <= 2.78*T
+            // 2*T = 6.95
 
-        integrate_rk4(
-            state_0,
-            exp_decay_system_dynamics_function,
-            step_size,
-            (10.0 / step_size) as usize,
-            &graph,
-            &color::BLUE,
-            &mut framebuffer,
-        );
+            integrate_rk4(
+                state_0,
+                exp_decay_system_dynamics_function,
+                step_size,
+                (10.0 / step_size) as usize,
+                &graph,
+                &color::BLUE,
+                &mut framebuffer,
+            );
+        }
+
+        if true {
+            // Graph the exact solution for sinusoidal oscillation.
+
+            graph.function(&oscillation, &color::WHITE, &mut framebuffer);
+
+            // Graph approximate solutions using our different integrators.
+
+            // let step_size = 2.0 * PERIOD;
+            // let step_size = PERIOD;
+            // let step_size = PERIOD / 2.0;
+            // let step_size = PERIOD / 4.0;
+            // let step_size = PERIOD / 8.0;
+            let step_size = PERIOD / 16.0;
+            // let step_size = PERIOD / 128.0;
+
+            let state_0 = State {
+                f0: MAGNITUDE,
+                f1: 0.0,
+            };
+
+            // Approximate the solution using basic Euler (O(h^2)).
+
+            integrate_euler(
+                state_0,
+                oscillation_system_dynamics_function,
+                step_size,
+                (20.0 / step_size) as usize,
+                &graph,
+                &color::YELLOW,
+                &mut framebuffer,
+            );
+
+            integrate_rk2(
+                state_0,
+                oscillation_system_dynamics_function,
+                step_size,
+                (20.0 / step_size) as usize,
+                &graph,
+                &color::GREEN,
+                &mut framebuffer,
+            );
+
+            integrate_rk4(
+                state_0,
+                oscillation_system_dynamics_function,
+                step_size,
+                (20.0 / step_size) as usize,
+                &graph,
+                &color::BLUE,
+                &mut framebuffer,
+            );
+        }
 
         Ok(framebuffer.get_all().clone())
     };
