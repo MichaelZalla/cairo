ee21aad11f45a3869032aeadde17a5c4327eb5c7
Michael Zalla <michael@zalla.io>
2024-09-14 11:26:59 -0700

    Encodes torsional springs for each `Face` in `SpringyMesh`;
    Adds `SpringyMesh::new_box()` constructor;

--- examples/springy-meshes/make_simulation.rs
+++ examples/springy-meshes/make_simulation.rs
@@ -20,48 +20,51 @@ static GRAVITY: Force = |_state: &StateVector, _i: usize, _current_time: f32| ->
 pub fn make_simulation<'a>() -> Simulation<'a> {
     let forces = vec![&GRAVITY];
 
-    let mesh = {
-        static POINT_SPACING_METERS: f32 = 3.0;
-        static NUM_POINTS: usize = 8;
+    // let mesh = {
+    //     static POINT_SPACING_METERS: f32 = 3.0;
+    //     static NUM_POINTS: usize = 8;
 
-        let mut points: Vec<Point> = vec![];
+    //     let mut points: Vec<Point> = vec![];
 
-        let mut x: f32 = 0.0;
-        let mut y: f32 = 16.0;
+    //     let mut x: f32 = 0.0;
+    //     let mut y: f32 = 16.0;
 
-        for i in 0..NUM_POINTS {
-            if i % 2 == 0 {
-                y -= POINT_SPACING_METERS;
-            } else {
-                x += POINT_SPACING_METERS;
-            };
+    //     for i in 0..NUM_POINTS {
+    //         if i % 2 == 0 {
+    //             y -= POINT_SPACING_METERS;
+    //         } else {
+    //             x += POINT_SPACING_METERS;
+    //         };
 
-            points.push(Point {
-                is_static: if i == 0 { true } else { false },
-                position: Vec3 {
-                    x,
-                    y,
-                    ..Default::default()
-                },
-                ..Default::default()
-            })
-        }
+    //         points.push(Point {
+    //             is_static: if i == 0 { true } else { false },
+    //             // mass: 2.5,
+    //             position: Vec3 {
+    //                 x,
+    //                 y,
+    //                 ..Default::default()
+    //             },
+    //             ..Default::default()
+    //         })
+    //     }
 
-        let mut struts: Vec<(usize, usize)> = vec![];
+    //     let mut struts: Vec<(usize, usize)> = vec![];
 
-        for i in 0..NUM_POINTS - 1 {
-            struts.push((i, i + 1));
-        }
+    //     for i in 0..NUM_POINTS - 1 {
+    //         struts.push((i, i + 1));
+    //     }
 
-        if NUM_POINTS > 2 {
-            let i = NUM_POINTS - 3;
-            let j = NUM_POINTS - 1;
+    //     if NUM_POINTS > 2 {
+    //         let i = NUM_POINTS - 3;
+    //         let j = NUM_POINTS - 1;
 
-            struts.push((i, j));
-        }
+    //         struts.push((i, j));
+    //     }
 
-        SpringyMesh::new(points, struts)
-    };
+    //     SpringyMesh::new(points, struts)
+    // };
+
+    let mesh = SpringyMesh::new_box(Default::default(), 4.0);
 
     let ground_plane_y: f32 = -10.0;
     let ground_plane_width: f32 = 60.0;
--- examples/springy-meshes/springy_mesh.rs
+++ examples/springy-meshes/springy_mesh.rs
@@ -1,19 +1,25 @@
+use std::f32::consts::PI;
+
+use cairo::vec::vec3::Vec3;
+
 use crate::{point::Point, strut::Strut};
 
 static STRENGTH_PER_UNIT_LENGTH: f32 = 100.0;
 static DAMPER_PER_UNIT_LENGTH: f32 = 100.0;
 
-// #[derive(Default, Debug, Copy, Clone)]
-// pub struct Face {
-//     pub struts: [usize; 3],
-//     pub vertex_angles: [f32; 3],
-// }
+#[derive(Default, Debug, Copy, Clone)]
+pub struct Face {
+    pub torsional_strength: f32,
+    pub torsional_damper: f32,
+    pub points: [usize; 3],
+    pub rest_angles: [f32; 3],
+}
 
 #[derive(Default, Debug, Clone)]
 pub struct SpringyMesh {
     pub points: Vec<Point>,
     pub struts: Vec<Strut>,
-    // pub faces: Vec<Face>,
+    pub faces: Vec<Face>,
 }
 
 impl SpringyMesh {
@@ -33,6 +39,71 @@ impl SpringyMesh {
             })
             .collect();
 
-        Self { points, struts }
+        let faces = vec![];
+
+        Self {
+            points,
+            struts,
+            faces,
+        }
+    }
+
+    pub fn new_box(center: Vec3, dimension: f32) -> Self {
+        let dimension_over_2 = dimension / 2.0;
+
+        let corners = vec![
+            // 0 - Top left
+            (-dimension_over_2, dimension_over_2),
+            // 1 - Top right
+            (dimension_over_2, dimension_over_2),
+            // 2 - Bottom right
+            (dimension_over_2, -dimension_over_2),
+            // 3 - Bottom left
+            (-dimension_over_2, -dimension_over_2),
+        ];
+
+        let points = corners
+            .into_iter()
+            .map(|c| Point {
+                position: Vec3 {
+                    x: center.x + c.0,
+                    y: center.y + c.1,
+                    z: 0.0,
+                },
+                ..Default::default()
+            })
+            .collect();
+
+        let struts = vec![
+            // 0 - Top
+            (0, 1),
+            // 1 - Right
+            (1, 2),
+            // 2 - Bottom
+            (3, 2),
+            // 3 - Left
+            (0, 3),
+            // 4 - Cross (top left to bottom right)
+            (0, 2),
+        ];
+
+        let mut mesh = Self::new(points, struts);
+
+        mesh.faces = vec![
+            Face {
+                points: [0, 1, 2], // Top-left, top-right, bottom-right.
+                rest_angles: [PI / 2.0, PI / 4.0, PI / 4.0],
+                torsional_strength: 100.0,
+                torsional_damper: 0.2,
+            },
+            Face {
+                points: [0, 3, 2], // Top-left, bottom-left, bottom-right.
+                rest_angles: [PI / 2.0, PI / 4.0, PI / 4.0],
+                torsional_strength: 100.0,
+                torsional_damper: 0.2,
+            },
+        ];
+
+        mesh
     }
 }
