fddf11eef896798034ab4261a44c4ff78179361a
Michael Zalla <michael@zalla.io>
2024-09-21 13:50:25 -0700

    Moves functions out of `impl` block;
    Passes `&forces` to `system_dynamics_function()`;

--- examples/rigid-body/simulation.rs
+++ examples/rigid-body/simulation.rs
@@ -27,9 +27,11 @@ impl<'a> Simulation<'a> {
             body.write_to(slice);
         }
 
-        let derivative = self.system_dynamics_function(&state, current_time, h);
+        let new_state = {
+            let derivative = system_dynamics_function(&state, &self.forces, current_time);
 
-        let new_state = forward_euler(&state, &derivative, h);
+            forward_euler(&state, &derivative, h)
+        };
 
         for (i, body) in self.rigid_bodies.iter_mut().enumerate() {
             let start = i * COEFFICIENT_COUNT;
@@ -40,68 +42,67 @@ impl<'a> Simulation<'a> {
             body.write_from(slice);
         }
     }
+}
 
-    fn system_dynamics_function(
-        &mut self,
-        current_state: &StateVector,
-        current_time: f32,
-        h: f32,
-    ) -> StateVector {
-        let n = current_state.0.len() / COEFFICIENT_COUNT;
+fn system_dynamics_function(
+    current_state: &StateVector,
+    forces: &[&Force],
+    current_time: f32,
+) -> StateVector {
+    let n = current_state.0.len() / COEFFICIENT_COUNT;
 
-        // Compute new accelerations (i.e., derivative) from net forces.
-        let mut derivative = self.compute_accelerations(current_state, current_time, h);
+    // Compute new accelerations (i.e., derivative) from net forces.
+    let mut derivative = compute_accelerations(current_state, forces, current_time);
 
-        for i in 0..n {
-            // Copy first derivatives (velocity, etc) from the prior state.
+    // Copy
+    for i in 0..n {
+        // Copy first derivatives (velocity, etc) from the prior state.
 
-            let start = i * COEFFICIENT_COUNT;
+        let start = i * COEFFICIENT_COUNT;
 
-            let one_over_mass = current_state.0[start];
+        let one_over_mass = current_state.0[start];
 
-            // Derive a velocity from the linear momentum.
+        // Derive a velocity from the linear momentum.
 
-            derivative.0[start + 1] = current_state.0[start + 8] * one_over_mass;
-            derivative.0[start + 2] = current_state.0[start + 9] * one_over_mass;
-            derivative.0[start + 3] = current_state.0[start + 10] * one_over_mass;
-        }
-
-        derivative
+        derivative.0[start + 1] = current_state.0[start + 8] * one_over_mass;
+        derivative.0[start + 2] = current_state.0[start + 9] * one_over_mass;
+        derivative.0[start + 3] = current_state.0[start + 10] * one_over_mass;
     }
 
-    fn compute_accelerations(
-        &mut self,
-        current_state: &StateVector,
-        current_time: f32,
-        _h: f32,
-    ) -> StateVector {
-        let size = current_state.0.len();
-        let n = size / COEFFICIENT_COUNT;
+    derivative
+}
 
-        let mut derivative = StateVector::new(size);
+fn compute_accelerations(
+    current_state: &StateVector,
+    forces: &[&Force],
+    current_time: f32,
+) -> StateVector {
+    let size = current_state.0.len();
+    let n = size / COEFFICIENT_COUNT;
 
-        // Compute net force and torque acting on each rigid body.
+    let mut derivative = StateVector::new(size);
 
-        // For each point, compute net environmental force acting on it.
-        for i in 0..n {
-            let mut net_force_acceleration: Vec3 = Default::default();
+    // Compute net force and torque acting on each rigid body.
 
-            for force in &self.forces {
-                net_force_acceleration += force(current_state, i, current_time);
-            }
+    // For each point, compute net environmental force acting on it.
+    for i in 0..n {
+        let mut net_force_acceleration: Vec3 = Default::default();
 
-            // Write the final net environmental acceleration.
-            let start = i * COEFFICIENT_COUNT;
-
-            // Linear momentum
-
-            derivative.0[start + 8] = net_force_acceleration.x;
-            derivative.0[start + 9] = net_force_acceleration.y;
-            derivative.0[start + 10] = net_force_acceleration.z;
+        for force in forces {
+            net_force_acceleration += force(current_state, i, current_time);
         }
 
-        derivative
+        // Write the final net environmental acceleration.
+        let start = i * COEFFICIENT_COUNT;
+
+        // Linear momentum
+
+        derivative.0[start + 8] = net_force_acceleration.x;
+        derivative.0[start + 9] = net_force_acceleration.y;
+        derivative.0[start + 10] = net_force_acceleration.z;
     }
+
+    derivative
 }
 
 fn forward_euler(current_state: &StateVector, derivative: &StateVector, h: f32) -> StateVector {
