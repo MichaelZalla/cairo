c0884cb3a46d904e95088fd77ed8d16ef88d4341
Michael Zalla <michael@zalla.io>
2024-09-28 12:30:45 -0700

    Implements `List::peek()` and `List::peek_mut()`;

--- src/collections/tree_new/mod.rs
+++ src/collections/tree_new/mod.rs
@@ -16,6 +16,14 @@ impl<T> List<T> {
         List { head: Link::None }
     }
 
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
+
     pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem,
@@ -75,5 +83,25 @@ mod test {
         // Check exhaustion.
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+
+        list.peek_mut().map(|value| *value = 42);
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.peek_mut(), Some(&mut 42));
     }
 }
