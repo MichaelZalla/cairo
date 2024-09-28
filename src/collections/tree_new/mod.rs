2bd91120a909e4f2ee27a791145f7db17f1641a2
Michael Zalla <michael@zalla.io>
2024-09-28 12:35:07 -0700

    Adds `IntoIter<T>` for `List`;
    Implements `Iterator` trait for `IntoIter<T>`;

--- src/collections/tree_new/mod.rs
+++ src/collections/tree_new/mod.rs
@@ -52,6 +52,22 @@ impl<T> Drop for List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -103,5 +119,21 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.peek_mut(), Some(&mut 42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
