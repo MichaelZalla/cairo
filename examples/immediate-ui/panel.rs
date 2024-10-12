fd50f980223ca780e70ee26767d6ce6e791dfed7
Michael Zalla <michael@zalla.io>
2024-10-12 11:45:34 -0700

    Renders an FPS counter;

--- examples/immediate-ui/panel.rs
+++ examples/immediate-ui/panel.rs
@@ -2,10 +2,13 @@ use std::fmt::Debug;
 
 use cairo::{
     serde::PostDeserialize,
-    ui::ui_box::{
-        tree::UIBoxTree,
-        utils::{button, container, spacer, text},
-        UIBoxFeatureFlag, UILayoutDirection,
+    ui::{
+        context::GLOBAL_UI_CONTEXT,
+        ui_box::{
+            tree::UIBoxTree,
+            utils::{button, container, spacer, text},
+            UIBox, UIBoxFeatureFlag, UILayoutDirection,
+        },
     },
 };
 
@@ -18,11 +21,15 @@ pub trait PanelInstance {
 #[derive(Clone)]
 pub(crate) struct SettingsPanel {
     id: String,
+    fps_average: f32,
 }
 
 impl SettingsPanel {
     pub fn from_id(id: &str) -> Self {
-        Self { id: id.to_string() }
+        Self {
+            id: id.to_string(),
+            fps_average: 0.0,
+        }
     }
 }
 
@@ -30,12 +37,26 @@ impl Debug for SettingsPanel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("SettingsPanel")
             .field("id", &self.id)
+            .field("fps_average", &self.fps_average)
             .finish()
     }
 }
 
 impl PostDeserialize for SettingsPanel {
     fn post_deserialize(&mut self) {}
+}
+
+impl SettingsPanel {
+    fn fps_counter(&self) -> UIBox {
+        let mut counter = text(
+            format!("SettingsPanel{}_fps_counter", self.id),
+            format!("FPS: {:.0}", self.fps_average),
+        );
+
+        counter.features |= UIBoxFeatureFlag::SkipTextCaching;
+
+        counter
+    }
 }
 
 impl PanelInstance for SettingsPanel {
@@ -45,6 +66,19 @@ impl PanelInstance for SettingsPanel {
 
             COMMAND_BUFFER.with(|buffer| -> Result<(), String> {
                 let mut pending_queue = buffer.pending_commands.borrow_mut();
+
+                // FPS counter
+
+                self.fps_average = {
+                    let new_fps =
+                        GLOBAL_UI_CONTEXT.with(|ctx| ctx.timing_info.borrow().frames_per_second);
+
+                    0.99 * self.fps_average + 0.01 * new_fps
+                };
+
+                tree.push(self.fps_counter())?;
+
+                tree.push(spacer(18))?;
 
                 // Setting: `clicked_count`
 
