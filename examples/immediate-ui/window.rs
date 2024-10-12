032fc681abf4ff23821ff640d11b0657a1bca3b7
Michael Zalla <michael@zalla.io>
2024-10-12 15:16:39 -0700

    Refactoring;

--- examples/immediate-ui/window.rs
+++ examples/immediate-ui/window.rs
@@ -12,6 +12,24 @@ use cairo::{
 
 use crate::panels::settings_panel::SettingsPanel;
 
+fn make_settings_panel(
+    id: &str,
+    arena: &mut Arena<SettingsPanel>,
+    render_callback: PanelRenderCallback,
+) -> Panel {
+    let settings_panel_instance_data = PanelInstanceData {
+        panel_instance: arena.insert(Uuid::new_v4(), SettingsPanel::from_id(id)),
+        render: Some(render_callback.clone()),
+        custom_render_callback: None,
+    };
+
+    Panel::new(
+        1.0,
+        Some(settings_panel_instance_data),
+        UILayoutDirection::TopToBottom,
+    )
+}
+
 pub(crate) fn make_window_list<'a>(
     settings_panel_arena: &mut Arena<SettingsPanel>,
     settings_panel_render_callback: PanelRenderCallback,
@@ -21,68 +39,62 @@ pub(crate) fn make_window_list<'a>(
 
     // Builds a main "window" that encompasses our app's native OS window.
 
-    let main_window = {
-        let window_id = "main_window".to_string();
-
+    let docked_window = {
+        let window_id = "docked_window".to_string();
+        let window_title = "Settings".to_string();
         let mut window_panel_tree = PanelTree::from_id(&window_id);
 
-        let settings_panel_instance_data = PanelInstanceData {
-            panel_instance: settings_panel_arena
-                .insert(Uuid::new_v4(), SettingsPanel::from_id("main")),
-            render: Some(settings_panel_render_callback.clone()),
-            custom_render_callback: None,
-        };
+        let panel_id = format!("{}_SettingsPanel", window_id);
 
-        let settings_panel = Panel::new(
-            1.0,
-            Some(settings_panel_instance_data),
-            UILayoutDirection::TopToBottom,
+        let panel = make_settings_panel(
+            panel_id.as_str(),
+            settings_panel_arena,
+            settings_panel_render_callback.clone(),
         );
 
-        window_panel_tree.push("SettingsPanel_main", settings_panel)?;
+        window_panel_tree.push(panel_id.as_str(), panel)?;
 
         Window::new(
             window_id,
-            "Settings".to_string(),
+            window_title,
             WindowOptions::docked(resolution),
             None,
             window_panel_tree,
         )
     };
 
-    list.0.push_back(main_window);
+    list.0.push_back(docked_window);
 
     // Builds a few non-native, "floating" windows that we can drag around.
 
     for i in 0..3 {
         let window_id = format!("floating_window_{}", i);
+        let window_title = "Settings".to_string();
 
         let mut window_panel_tree = PanelTree::from_id(&window_id);
 
-        let settings_panel_instance_data = PanelInstanceData {
-            panel_instance: settings_panel_arena.insert(
-                Uuid::new_v4(),
-                SettingsPanel::from_id(format!("{}", i).as_str()),
-            ),
+        let panel_id = format!("{}_SettingsPanel", window_id);
+
+        let panel_instance_data = PanelInstanceData {
+            panel_instance: settings_panel_arena
+                .insert(Uuid::new_v4(), SettingsPanel::from_id(panel_id.as_str())),
             render: Some(settings_panel_render_callback.clone()),
             custom_render_callback: None,
         };
-
-        let settings_panel = Panel::new(
+        let panel = Panel::new(
             1.0,
-            Some(settings_panel_instance_data),
+            Some(panel_instance_data),
             UILayoutDirection::TopToBottom,
         );
 
-        window_panel_tree.push(&format!("SettingsPanel_{}", i), settings_panel)?;
+        window_panel_tree.push(panel_id.as_str(), panel)?;
 
         list.0.push_back(Window::new(
             window_id,
-            "Settings".to_string(),
+            window_title,
             WindowOptions {
                 with_titlebar: true,
-                position: (50 + i * 50, 50 + i * 50),
-                size: (350, 640),
+                position: (50 + i * 250, 50),
                 ..Default::default()
             },
             None,
