08d73f840f24ce256cd28fc9090674fce22b4af4
Michael Zalla <michael@zalla.io>
2024-10-15 12:12:53 -0700

    Combines render callbacks;

--- examples/basic-window/main.rs
+++ examples/basic-window/main.rs
@@ -3,7 +3,10 @@ extern crate sdl2;
 use std::cell::RefCell;
 
 use cairo::{
-    app::{resolution::Resolution, App, AppWindowInfo},
+    app::{
+        resolution::{Resolution, RESOLUTION_1920_BY_1080},
+        App, AppWindowInfo,
+    },
     buffer::Buffer2D,
     device::{game_controller::GameControllerState, keyboard::KeyboardState, mouse::MouseState},
 };
@@ -11,18 +14,14 @@ use cairo::{
 fn main() -> Result<(), String> {
     let mut window_info = AppWindowInfo {
         title: "examples/basic-window".to_string(),
+        window_resolution: RESOLUTION_1920_BY_1080,
+        canvas_resolution: RESOLUTION_1920_BY_1080,
         ..Default::default()
     };
 
-    let render_scene_to_framebuffer = |_frame_index: Option<u32>,
-                                       _new_resolution: Option<Resolution>|
-     -> Result<Vec<u32>, String> { Ok(vec![]) };
-
-    let (app, _event_watch) = App::new(&mut window_info, &render_scene_to_framebuffer);
-
     // Set up our app
 
-    let framebuffer = Buffer2D::new(
+    let framebuffer: Buffer2D<u32> = Buffer2D::new(
         window_info.window_resolution.width,
         window_info.window_resolution.height,
         None,
@@ -30,13 +29,9 @@ fn main() -> Result<(), String> {
 
     let framebuffer_rc = RefCell::new(framebuffer);
 
-    let mut update = |_app: &mut App,
-                      _keyboard_state: &mut KeyboardState,
-                      _mouse_state: &mut MouseState,
-                      _game_controller_state: &mut GameControllerState|
-     -> Result<(), String> { Ok(()) };
-
-    let render = |_frame_index, _new_resolution| -> Result<Vec<u32>, String> {
+    let render_scene_to_framebuffer = |_frame_index: Option<u32>,
+                                       _new_resolution: Option<Resolution>|
+     -> Result<Vec<u32>, String> {
         let mut framebuffer = framebuffer_rc.borrow_mut();
 
         // Clears pixel buffer
@@ -49,7 +44,17 @@ fn main() -> Result<(), String> {
         return Ok(framebuffer.get_all().clone());
     };
 
-    app.run(&mut update, &render)?;
+    let (app, _event_watch) = App::new(&mut window_info, &render_scene_to_framebuffer);
+
+    // Set up our app
+
+    let mut update = |_app: &mut App,
+                      _keyboard_state: &mut KeyboardState,
+                      _mouse_state: &mut MouseState,
+                      _game_controller_state: &mut GameControllerState|
+     -> Result<(), String> { Ok(()) };
+
+    app.run(&mut update, &render_scene_to_framebuffer)?;
 
     Ok(())
 }
