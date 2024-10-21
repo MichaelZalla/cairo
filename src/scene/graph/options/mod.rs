9539e585bbcdf3a8a213e9d9cd98d3a9024101e4
Michael Zalla <michael@zalla.io>
2024-10-21 07:44:53 -0700

    Default flags for debug builds;

--- src/scene/graph/options/mod.rs
+++ src/scene/graph/options/mod.rs
@@ -2,10 +2,21 @@ use serde::{Deserialize, Serialize};
 
 use crate::resource::handle::Handle;
 
-#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SceneGraphRenderOptions {
     pub draw_lights: bool,
     pub draw_cameras: bool,
     pub draw_shadow_map_cameras: bool,
     pub camera: Option<Handle>,
+}
+
+impl Default for SceneGraphRenderOptions {
+    fn default() -> Self {
+        Self {
+            draw_lights: cfg!(debug_assertions),
+            draw_cameras: cfg!(debug_assertions),
+            draw_shadow_map_cameras: false,
+            camera: None,
+        }
+    }
 }
