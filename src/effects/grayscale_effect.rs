8a191e644d8c135ebfdff6b3bf30b009976ff1ed
Michael Zalla <michael@zalla.io>
2024-11-23 09:38:34 -0800

    Uses iterators;

--- src/effects/grayscale_effect.rs
+++ src/effects/grayscale_effect.rs
@@ -1,40 +1,31 @@
-use crate::{buffer::Buffer2D, color::Color, effect::Effect, vec::vec3::Vec3};
+use crate::{buffer::Buffer2D, color::Color, effect::Effect, vec::vec3};
 
 #[derive(Default)]
 pub struct GrayscaleEffect {}
 
 impl Effect for GrayscaleEffect {
     fn apply(&mut self, buffer: &mut Buffer2D) {
-        for y in 0..buffer.height {
-            for x in 0..buffer.width {
-                let color = Color::from_u32(*buffer.get(x, y));
+        for color_u32 in buffer.data.iter_mut() {
+            let color = Color::from_u32(*color_u32);
 
-                let mut color_vec3 = color.to_vec3() / 255.0;
+            let mut color_vec3 = color.to_vec3() / 255.0;
 
-                // Transform to linear space.
-                color_vec3 *= color_vec3;
+            // Transform to linear space.
 
-                // Apply channel-weighted grayscale transform.
-                let weighted_average =
-                    color_vec3.x * 0.2126 + color_vec3.y * 0.7152 + color_vec3.z * 0.0722;
+            color_vec3 *= color_vec3;
 
-                let mut grayscale_vec3 = Vec3 {
-                    x: weighted_average,
-                    y: weighted_average,
-                    z: weighted_average,
-                };
+            // Apply channel-weighted grayscale transform.
 
-                // Transform back to non-linear (gamma) space.
-                grayscale_vec3 = Vec3 {
-                    x: grayscale_vec3.x.sqrt(),
-                    y: grayscale_vec3.y.sqrt(),
-                    z: grayscale_vec3.z.sqrt(),
-                };
+            let weighted_average_linear =
+                color_vec3.x * 0.2126 + color_vec3.y * 0.7152 + color_vec3.z * 0.0722;
 
-                let grayscale = Color::from_vec3(grayscale_vec3 * 255.0);
+            // Transform back to non-linear (gamma) space.
 
-                buffer.set(x, y, grayscale.to_u32());
-            }
+            let weighted_average_nonlinear = weighted_average_linear.sqrt();
+
+            let grayscale = Color::from_vec3(vec3::ONES * weighted_average_nonlinear * 255.0);
+
+            *color_u32 = grayscale.to_u32();
         }
     }
 }
--- src/effects/invert_effect.rs
+++ src/effects/invert_effect.rs
@@ -5,18 +5,16 @@ pub struct InvertEffect {}
 
 impl Effect for InvertEffect {
     fn apply(&mut self, buffer: &mut Buffer2D) {
-        for y in 0..buffer.height {
-            for x in 0..buffer.width {
-                let color = Color::from_u32(*buffer.get(x, y));
+        for color_u32 in buffer.data.iter_mut() {
+            let color = Color::from_u32(*color_u32);
 
-                let inverse = Color::rgb(
-                    255 - color.r as u8,
-                    255 - color.g as u8,
-                    255 - color.b as u8,
-                );
+            let inverse = Color::rgb(
+                (255.0 - color.r) as u8,
+                (255.0 - color.g) as u8,
+                (255.0 - color.b) as u8,
+            );
 
-                buffer.set(x, y, inverse.to_u32());
-            }
+            *color_u32 = inverse.to_u32();
         }
     }
 }
