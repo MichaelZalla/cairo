9f833ae53602ab80439c5a17795f51a4657b5fed
Michael Zalla <michael@zalla.io>
2024-11-24 15:11:55 -0800

    Computes signed distance using constant-normal form;

--- src/geometry/primitives/plane.rs
+++ src/geometry/primitives/plane.rs
@@ -4,15 +4,19 @@ use crate::vec::vec3::Vec3;
 
 #[derive(Default, Debug, Copy, Clone, Serialize, Deserialize)]
 pub struct Plane {
+    // Constant-normal form.
     pub normal: Vec3, // Plane normal.
     pub point: Vec3,  // A point on the plane.
+    pub d: f32,       // d = dot(n, P) for any point P on the plane.
 }
 
 impl Plane {
     pub fn new(point: Vec3, direction: Vec3) -> Self {
         let normal = direction.as_normal();
 
-        Self { normal, point }
+        let d = normal.dot(point);
+
+        Self { normal, d, point }
     }
 
     pub fn is_on_or_in_front_of(&self, position: &Vec3, radius: f32) -> bool {
@@ -20,6 +24,6 @@ impl Plane {
     }
 
     pub fn get_signed_distance(&self, position: &Vec3) -> f32 {
-        self.normal.dot(*position - self.point)
+        self.normal.dot(*position) - self.d
     }
 }
