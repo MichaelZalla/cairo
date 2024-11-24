6fc521187cb31ef5a1f89b55286f2f92493a5f2a
Michael Zalla <michael@zalla.io>
2024-11-24 15:10:24 -0800

    Adds `Plane::new()`;

--- src/geometry/primitives/plane.rs
+++ src/geometry/primitives/plane.rs
@@ -4,11 +4,17 @@ use crate::vec::vec3::Vec3;
 
 #[derive(Default, Debug, Copy, Clone, Serialize, Deserialize)]
 pub struct Plane {
-    pub point: Vec3,
-    pub normal: Vec3,
+    pub normal: Vec3, // Plane normal.
+    pub point: Vec3,  // A point on the plane.
 }
 
 impl Plane {
+    pub fn new(point: Vec3, direction: Vec3) -> Self {
+        let normal = direction.as_normal();
+
+        Self { normal, point }
+    }
+
     pub fn is_on_or_in_front_of(&self, position: &Vec3, radius: f32) -> bool {
         self.get_signed_distance(position) > -radius
     }
