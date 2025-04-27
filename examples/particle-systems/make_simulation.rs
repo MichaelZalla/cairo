4ce5d9a9ee314942f13ebc014a06e7431422bb7b
Michael Zalla <michael@zalla.io>
2025-04-27 06:55:08 -0700

    Uses library static;

--- examples/particle-systems/make_simulation.rs
+++ examples/particle-systems/make_simulation.rs
@@ -2,14 +2,11 @@ use std::cell::RefCell;
 
 use cairo::{
     physics::simulation::{
-        force::{ContactPoint, PointForce},
+        force::gravity::GRAVITY_POINT_FORCE,
         particle::{
             generator::{ParticleGenerator, ParticleGeneratorKind},
             Particle,
         },
-        physical_constants::EARTH_GRAVITY,
-        state_vector::StateVector,
-        units::Newtons,
     },
     random::sampler::RandomSampler,
     vec::vec3,
@@ -20,13 +17,6 @@ use crate::simulation::Simulation;
 pub(crate) const SEED_SIZE: usize = 2048;
 
 pub static PARTICLE_MAX_AGE_SECONDS: f32 = 10.0;
-
-static GRAVITY_POINT_FORCE: PointForce =
-    |_state: &StateVector, _i: usize, _current_time: f32| -> (Newtons, Option<ContactPoint>) {
-        let newtons = -vec3::UP * EARTH_GRAVITY;
-
-        (newtons, None)
-    };
 
 pub fn make_simulation(sampler: RefCell<RandomSampler<SEED_SIZE>>) -> Simulation<SEED_SIZE> {
     let mass = 50.0;
