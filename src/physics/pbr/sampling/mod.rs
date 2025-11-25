07a60d2465cb3b7b726c8e07fbe435f4f94373f0
Michael Zalla <michael@zalla.io>
2025-11-25 13:12:33 -0800

    Uses `Vec3::basis()`;

--- src/physics/pbr/sampling/mod.rs
+++ src/physics/pbr/sampling/mod.rs
@@ -1,9 +1,6 @@
 use std::f32::consts::TAU;
 
-use crate::vec::{
-    vec2::Vec2,
-    vec3::{self, Vec3},
-};
+use crate::vec::{vec2::Vec2, vec3::Vec3};
 
 pub fn importance_sample_ggx(x_i: Vec2, normal: &Vec3, roughness: f32) -> Vec3 {
     let alpha = roughness * roughness;
@@ -24,19 +21,11 @@ pub fn importance_sample_ggx(x_i: Vec2, normal: &Vec3, roughness: f32) -> Vec3 {
 
     // Tangent space to world space.
 
-    let up = if normal.z.abs() < 0.999 {
-        vec3::UP
-    } else {
-        vec3::RIGHT
-    };
+    let (forward, right, up) = normal.basis();
 
-    let tangent = up.cross(*normal).as_normal();
-
-    let bitangent = normal.cross(tangent);
-
-    let biased_sample = tangent * tangent_space_halfway.x
-        + bitangent * tangent_space_halfway.y
-        + *normal * tangent_space_halfway.z;
+    let biased_sample = right * tangent_space_halfway.x
+        + up * tangent_space_halfway.y
+        + forward * tangent_space_halfway.z;
 
     biased_sample.as_normal()
 }
